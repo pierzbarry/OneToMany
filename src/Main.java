4083e6cb48363b79f66a2e53880b6fcb04400fd7
pierz barry <pmbarry@asu.edu>
2019-11-13 21:25:19 -0500

    add user and pass verification

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,32 @@
 // be able to edit the company.name, person.name
 // edit connection between one person and one company (changing a person works for company) 
 
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    // poor mans database
     private static ArrayList<Company> companyDB = new ArrayList<>();
     private static ArrayList<Person> personDB = new ArrayList<>();
     private static ArrayList<Link> linkDB = new ArrayList<>();
+    private static ArrayList<User> userDB = new ArrayList<>();
 
     private static Scanner keyboard = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
 
         // calls the database constructor methods
         createCompanyDB();
         createPersonDB();
         createLinkDB();
+        createUserDB();
 
         // display our constructed information
-        showAllLinks();
-        
+        // showAllLinks();
+
+        // require user login to use database app
+        validateLogIn();
+
         // adding user information
         String userStr;
 
@@ -33,8 +38,9 @@ public class Main {
                     "\t[\"P\"] add a Person\n" +
                     "\t[\"C\"] add a Company\n" +
                     "\t[\"L\"] add a Link\n" +
-                    "\t[\"E\"] to edit some information\n\n" +
-                    "\t[\"Q\"] to Quit\n\n" +
+                    "\t[\"E\"] to edit some information\n" +
+                    "\t[\"D\"] to display a database\n\n" +
+                    "\t[\"Q\"] to Logout\n\n" +
                     "Your Choice: ");
             userStr = keyboard.nextLine();
 
@@ -49,10 +55,11 @@ public class Main {
                 showAllLinksByCompany();
             } else if (userStr.equalsIgnoreCase("E")) {
                 // edit info
-                System.out.println("Would you like to edit a Person (\"P\") Company (\"C\") or Quit (\"Q\")");
+                System.out.println("What database would you like to edit? " +
+                        "\nPerson [\"P\"] Company [\"C\"] or Quit [\"Q\"]: ");
                 userStr = keyboard.nextLine();
 
-                if(userStr.equalsIgnoreCase("P")) {
+                if (userStr.equalsIgnoreCase("P")) {
                     editPerson();
                 } else if (userStr.equalsIgnoreCase("C")) {
                     editCompany();
@@ -60,8 +67,24 @@ public class Main {
                     break;
                 }
 
+            } else if (userStr.equalsIgnoreCase("D")) {
+                System.out.print("Which database would you like to display? " +
+                        "\nPerson [\"P\"] Company [\"C\"] List [\"L\"] Quit [\"Q\"]: ");
+                userStr = keyboard.nextLine();
+
+                if (userStr.equalsIgnoreCase("P")) {
+                    showPersonDB();
+                } else if (userStr.equalsIgnoreCase("C")) {
+                    showCompanyDB();
+                } else if (userStr.equalsIgnoreCase("L")) {
+                    showAllLinksByCompany();
+                } else if (userStr.equalsIgnoreCase("Q")) {
+                    break;
+                }
+
             } else if (userStr.equalsIgnoreCase("Q")) {
-                break;
+                System.out.println("\nYou have logged out! See ya later <3");
+                validateLogIn();
             }
         } while (true);
 
@@ -69,7 +92,7 @@ public class Main {
         keyboard.close();
     }
 
-    private static void editPerson(){
+    private static void editPerson() {
         System.out.println("Enter the ID of the person you would like to edit...");
         showPersonDB();
         System.out.print("Your choice: ");
@@ -81,14 +104,14 @@ public class Main {
 
         // find the person they entered and REMEMBER IT (to actual person object)
         for (Person person : personDB) {
-            if(person.getId() == userId) {
+            if (person.getId() == userId) {
                 myPerson = person;
             }
         }
 
         // assuming we found it and remembered it
         // replace said name with user input
-        if(myPerson != null) {
+        if (myPerson != null) {
             System.out.print("Please enter a new Name: ");
             String userStr = keyboard.nextLine();
             myPerson.setName(userStr);
@@ -100,7 +123,7 @@ public class Main {
         }
     }
 
-    private static void editCompany(){
+    private static void editCompany() {
         System.out.println("Enter the ID of the company you would like to edit...");
         showCompanyDB();
         System.out.print("Your choice: ");
@@ -110,12 +133,12 @@ public class Main {
         Company myCompany = null;
 
         for (Company company : companyDB) {
-            if(company.getId() == userId) {
-               myCompany = company;
+            if (company.getId() == userId) {
+                myCompany = company;
             }
         }
 
-        if(myCompany != null) {
+        if (myCompany != null) {
             System.out.print("Please enter a new Name: ");
             String userStr = keyboard.nextLine();
             myCompany.setName(userStr);
@@ -127,25 +150,27 @@ public class Main {
         }
     }
 
-    private static void createNewPerson(){
+    private static void createNewPerson() {
         String name;
         System.out.print("\nPlease enter the Person's name: ");
         name = keyboard.nextLine();
 
-        int id = personDB.size()+1;
+        // setting person's id to be one larger than the current largest (thus making each id unique)
+        // i.e if the last personID is 10, the new one will be 11
+        int id = personDB.size() + 1;
         personDB.add(new Person(id, name));
     }
 
-    private static void createNewCompany(){
+    private static void createNewCompany() {
         String name;
         System.out.print("\nPlease enter the Company's name: ");
         name = keyboard.nextLine();
 
-        int id = companyDB.size()+1;
+        int id = companyDB.size() + 1;
         companyDB.add(new Company(id, name));
     }
 
-    private static void createNewLink(){
+    private static void createNewLink() {
         int personId;
         System.out.println("\nPlease enter the ID of the Person you wish to link...");
         showPersonDB();
@@ -170,27 +195,28 @@ public class Main {
         String role = keyboard.nextLine();
 
         System.out.println();
-        int id = linkDB.size()+1;
+        int id = linkDB.size() + 1;
         linkDB.add(new Link(id, companyId, personId, startDate, role));
     }
 
-    private static void showPersonDB(){
+    private static void showPersonDB() {
+        // for loop goes through database array and prints its contents
         System.out.println("Person Database: ");
-        for(Person person : personDB) {
+        for (Person person : personDB) {
             System.out.println("\tId: " + person.getId() + " Name: " + person.getName());
         }
         System.out.println();
     }
 
-    private static void showCompanyDB(){
+    private static void showCompanyDB() {
         System.out.println("Company Database: ");
-        for(Company company : companyDB) {
+        for (Company company : companyDB) {
             System.out.println("\tId: " + company.getId() + " Name: " + company.getName());
         }
         System.out.println();
     }
-    
-    private static void showAllLinks(){
+
+    private static void showAllLinks() {
         System.out.println("Link Database: ");
 
         // setting outside forloop variable to create memory
@@ -212,9 +238,10 @@ public class Main {
         }
     }
 
-    private static void showAllLinksByCompany(){
-
-        for(Company company : companyDB) {
+    private static void showAllLinksByCompany() {
+        // same concept as showAllLinks but organizes by company
+        System.out.println();
+        for (Company company : companyDB) {
             System.out.println(company.getName() + ": ");
             // loop through all links
             for (Link link : linkDB) {
@@ -223,7 +250,7 @@ public class Main {
                     // loop through all people
                     for (Person person : personDB) {
                         // see if person belongs to current link
-                        if ( person.getId() == link.getPersonId()) {
+                        if (person.getId() == link.getPersonId()) {
                             System.out.println("\t" + person.getName() + " :: " + link.getStartDate() + ", " + link.getRole() + ".");
                         }
                     }
@@ -233,35 +260,35 @@ public class Main {
         }
     }
 
-    private static String LookUpCompanyName(int companyId){
+    private static String LookUpCompanyName(int companyId) {
         for (Company company : companyDB) {
-            if(company.getId() == companyId) {
+            if (company.getId() == companyId) {
                 return company.getName();
             }
         }
         return "Unknown";
     }
 
-    private static String LookUpPersonName(int personId){
+    private static String LookUpPersonName(int personId) {
         for (Person person : personDB) {
-            if(person.getId() == personId) {
+            if (person.getId() == personId) {
                 return person.getName();
             }
         }
         return "Unknown";
     }
-        
-    private static void createCompanyDB(){
-        
+
+    private static void createCompanyDB() {
+
         // create companies
         companyDB.add(new Company(1, "Apple"));
         companyDB.add(new Company(2, "Waffle House"));
         companyDB.add(new Company(3, "Starbucks"));
         companyDB.add(new Company(4, "Unemployed"));
     }
-    
-    private static void createPersonDB(){
-        
+
+    private static void createPersonDB() {
+
         // create people
         personDB.add(new Person(1, "Karen"));
         personDB.add(new Person(2, "Pierz"));
@@ -274,12 +301,13 @@ public class Main {
         personDB.add(new Person(9, "Jane"));
         personDB.add(new Person(10, "Ben"));
     }
-    
-    private static void createLinkDB(){
+
+    private static void createLinkDB() {
         // 1 == Apple || 2 == Waffle House || 3 == Starbucks || 4 == Unemployed
+
         // 1 == Karen || 2 == Pierz || 3 == Molly || 4 == Josh || 5 == Kyle
         // 6 == Mac || 7 == Brandy || 8 == Anne || 9 == Jane || 10 == Ben
-        
+
         // create links between companies and people
         linkDB.add(new Link(1, 1, 1, "June 2018", "Front-End Developer"));
         linkDB.add(new Link(2, 1, 2, "July 2019", "Janitor"));
@@ -287,12 +315,60 @@ public class Main {
 
         linkDB.add(new Link(4, 3, 4, "February 2009", "Barista"));
         linkDB.add(new Link(6, 3, 6, "December 2017", "Barista"));
-        linkDB.add(new Link(5, 3, 5, "January 2015", "Shift Lead" ));
+        linkDB.add(new Link(5, 3, 5, "January 2015", "Shift Lead"));
 
         linkDB.add(new Link(7, 2, 7, "June 2020", "Cook"));
         linkDB.add(new Link(3, 2, 3, "March 2002", "Server"));
         linkDB.add(new Link(9, 2, 9, "October 2016", "Server"));
 
         linkDB.add(new Link(10, 4, 8, "January 2020", "Laid Off"));
-    }    
-}
+    }
+
+    private static void createUserDB(){
+
+        // create user login information
+        userDB.add(new User("pierz", "pass"));
+        userDB.add(new User("user", "word"));
+    }
+
+    private static boolean validateLogIn() {
+        // requires user to input valid username and password to use app
+
+        String userName, userPassword;
+        int counter = 0;
+
+        while (true) {
+
+            System.out.println("\n|▓▓▓【☆】▓▓▓| Database Builder |▓▓▓【☆】▓▓▓|");
+
+            System.out.print("Please enter a valid username: ");
+            userName = keyboard.nextLine();
+            System.out.print("Please enter a valid password: ");
+            userPassword = keyboard.nextLine();
+
+
+            // loops through entire userDB array and compares keyboard input to it
+            for (User u : userDB) {
+                // returns true (thus ending loop) if a matching username and password combination are found
+                if (u.getName().equals(userName)) {
+                    if (u.getPassword().equals(userPassword)) {
+                        System.out.println("\nYou have successfully logged in!");
+                        return true;
+                    }
+                }
+            }
+
+            // multiple attempt limiter if desired
+            // if you input this make sure you edit beginning of program because otherwise after
+            // three failed attempts it will log you in lol!
+
+//            counter++;
+//            if (counter < 3) {
+//                System.out.println("Invalid login. Please try again. ");
+//            } else {
+//                System.out.println("Three invalid logins. Sorry! Try again another day.");
+//                return false;
+//            }
+        }
+    }
+} // closes main
